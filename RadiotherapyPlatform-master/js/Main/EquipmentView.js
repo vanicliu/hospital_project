163b35feeed3780cd38803bc8f666093fe0ac0fc
vanicliu <2388130357@qq.com>
2018-06-12 21:50:44 +0800

    a

--- RadiotherapyPlatform-master/js/Main/EquipmentView.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var session = getSession();
-    if (session.role != "物理师" && session.role != "模拟技师" && session.role != "治疗技师") {
-         $("#Menu-EquipmentView").attr("href", "javascript:;");
-         $("#Menu-EquipmentView").bind("click", function(){
-            alert("权限不够！");
-         });
-    }
-});
